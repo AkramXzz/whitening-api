75d78a56d52fb9dc6b23f14f01517aef87cc5693
AkramXzz <c2169099@gmail.com>
2026-02-21 02:50:16 +0800

    Create whitening.js

---
+++ api/whitening.js
@@ -0,0 +1,43 @@
+import sharp from "sharp";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  const apikey = req.query.apikey;
+
+  if (apikey !== "akram123") {
+    return res.status(403).json({ status: false, message: "Invalid API Key" });
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  let buffers = [];
+
+  req.on("data", chunk => {
+    buffers.push(chunk);
+  });
+
+  req.on("end", async () => {
+    try {
+      const imageBuffer = Buffer.concat(buffers);
+
+      const output = await sharp(imageBuffer)
+        .modulate({
+          brightness: 1.3,
+          saturation: 1.1
+        })
+        .toBuffer();
+
+      res.setHeader("Content-Type", "image/jpeg");
+      res.status(200).send(output);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+          }
